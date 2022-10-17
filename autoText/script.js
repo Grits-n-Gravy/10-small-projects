78a32623f24ebfdaaebf76b24a01fb62fa5fa50a
christian <christianjones94@gmail.com>
2022-10-17 14:04:57 -0400

    initial commit

---
+++ autoText/script.js
@@ -0,0 +1,13 @@
+const text = "remember those LED belts from the early 2000's?";
+
+let index = 0;
+
+function writeText() {
+  document.body.innerText = text.slice(0, index);
+  index++;
+  if (index > text.length) {
+    index = 0;
+  }
+}
+
+setInterval(writeText, 100);
